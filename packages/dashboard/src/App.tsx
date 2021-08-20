b5492ee8e454d058fe9e98606aa46efbb957ab2a
Nguyen Tran <nguyentranthepog@gmail.com>
2021-08-20 14:56:42 +0700

    feat: add react-router-dom

--- packages/dashboard/src/App.tsx
+++ packages/dashboard/src/App.tsx
@@ -1,20 +1,50 @@
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import { Button } from '@todo-liszt/components';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-
-        <Button>Shared Button</Button>
-      </header>
-    </div>
+    <Switch>
+      <Route path="/app/settings">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Settings Page</p>
+            <Button>Shared Button</Button>
+            <Link to="/app/lists">Go to settings</Link>
+            <Link to="/app">Go to home</Link>
+          </header>
+        </div>
+      </Route>
+      <Route path="/app/lists">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Lists Page</p>
+            <Button>Shared Button</Button>
+            <Link to="/app/settings">Go to settings</Link>
+            <Link to="/app">Go to home</Link>
+          </header>
+        </div>
+      </Route>
+      <Route path="/app">
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>Home page</p>
+            <Button>Shared Button</Button>
+            <a href="/">Go to landing page</a>
+            <Link to="/app/settings">Go to settings</Link>
+            <Link to="/app/lists">Go to lists</Link>
+          </header>
+        </div>
+      </Route>
+      <Route path="*">
+        <h1>Not Found</h1>
+      </Route>
+    </Switch>
   );
 }
 
--- packages/guest/pages/index.tsx
+++ packages/guest/pages/index.tsx
@@ -1,14 +1,41 @@
+import Link from 'next/link';
 import type { NextPage } from 'next';
 import styles from '../styles/Home.module.css';
 
 import { Button } from '@todo-liszt/components';
 
+// const APP_DOMAIN = 'http://localhost:3001';
+
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Button>Shared button</Button>
+      <a href="/app">Go to dashboard (CRA)</a>
+      <Link href="/about">Go to about page (NextJS)</Link>
     </div>
   );
 };
 
 export default Home;
+
+// location / {
+//   proxy_pass "http://localhost:3000";
+//   proxy_http_version 1.1;
+//   proxy_set_header Upgrade $http_upgrade;
+//   proxy_set_header Connection 'upgrade';
+//   proxy_set_header Host $host;
+//   proxy_cache_bypass $http_upgrade;
+// }
+
+// server {
+//   listen 80;
+//   listen [::]:80;
+
+//   server_name _;
+
+//   location /app {
+// root /var/www/todo-liszt/dashboard/build;
+// index index.html index.htm;
+//           try_files $uri /index.html =404;
+//   }
+// }
